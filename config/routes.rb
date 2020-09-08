d8a4f03b522b03995ef3f9353ff97277c4a36f4d
Dikiy <denis.dikiy@gmail.com>
2020-09-08 14:54:49 +0300

    Finish static pages excercises

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/home'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
