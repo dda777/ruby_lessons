e4a3f49e56f339a800c4cb8667be69876a5f69dd
Dikiy <denis.dikiy@gmail.com>
2020-09-18 13:56:37 +0300

    Add account activations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,9 +39,11 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com'}
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.action_mailer.default charset: 'utf-8'
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
