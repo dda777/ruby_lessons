c4c67f11f51f541726174422770c38ea3fb83fa4
Denis <denis.dikiy@gmail.com>
2020-09-21 08:29:52 +0300

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
@@ -9,5 +11,6 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -17,23 +17,24 @@ class UserMailerTest < ActionMailer::TestCase
   # end
 
   test "account_activation" do
-    user = users(:one)
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
+    @user.activation_token = User.new_token
+    mail = UserMailer.account_activation(@user)
     assert_equal "Account activation", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name, mail.text_part.body.to_s.encode("UTF-8")
-    assert_match user.activation_token, mail.body.encoded
-    assert_match CGI.escape(user.email), mail.body.encoded
+    assert_equal [@user.email], mail.to
+    assert_equal ["denis.dikiy@gmail.com"], mail.from
+    assert_match @user.name, mail.text_part.body.to_s.encode("UTF-8")
+    assert_match @user.activation_token, mail.body.encoded
+    assert_match CGI.escape(@user.email), mail.body.encoded
   end
 
-  # test "password_reset" do
-  #   mail = UserMailer.password_reset
-  #   assert_equal "Password reset", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "password_reset" do
+    @user.reset_token = User.new_token
+    mail = UserMailer.password_reset(@user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 
 end
