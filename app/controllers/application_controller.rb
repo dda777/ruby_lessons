6a7cc4b0cd533917d78603753c8845849f549990
Dikiy <denis.dikiy@gmail.com>
2020-10-22 15:09:26 +0300

    Add swith locale method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  around_action :switch_locale
   include SessionsHelper
 
+  def switch_locale(&action)
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_header
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.with_locale(locale, &action)
+  end
+
   private
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
@@ -16,7 +25,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
   def default_url_options
-    { locale: I18n.locale }
+    {locale: I18n.locale}
   end
+
+  def extract_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
 end
